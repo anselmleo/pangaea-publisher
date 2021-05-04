cd44979232337b247b29424859f1238cf3241403
Anselm <anselmleo@gmail.com>
2021-05-04 16:13:46 +0100

    update package.json

--- startup/routes.ts
+++ startup/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import reqLogger from '../startup/logger';
 import error from '../middleware/error';
+import auth from '../middleware/auth';
 import subscribers from '../routes/subscribers';
 import topics from '../routes/topics';
 import publish from '../routes/publish';
@@ -9,6 +10,7 @@ export default (app: express.Application) => {
   app.use(express.json());
   app.use(reqLogger);
   app.use(error);
+  app.use(auth);
   app.use('/api/subscribers', subscribers);
   app.use('/api/topics', topics);
   app.use('/api/publish', publish);
